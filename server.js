b4b5fc46d34fc09c63607ad05113bc7e92fa19e3
pittsblake <pittsblake1@gmail.com>
2017-10-12 10:59:09 -0400

    Created a basic express server js and connected to mongoose

---
+++ server.js
@@ -0,0 +1,33 @@
+//Imports variables from .env file
+require ('dotenv').config()
+const express = require ('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+//Create new app using EXpress
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+const connection = mongoose.connection
+
+connection.on('connected', () => {
+    console.log("Successfully connected to MONGODB")
+})
+
+connection.on('error', (err) => {
+    console.log('MongoDB ERROR: ', err)
+})
+
+
+//Inject middleware
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("App listening on port", PORT)
+})
